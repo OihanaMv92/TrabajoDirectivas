90dac3cb8a18dab92617df2db4beec2820b00e7e
Oihana Mendez <oihana@gainkode.com>
2022-04-26 10:51:44 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'trabajoDirectiva';
+  title = 'Registros de Usuarios';
+  mensaje="";
+  registro=false;
+nombre:string="";
+apellido:string="";
+entradas:any[];
+
+
+// Esti simula una bd.
+constructor() {
+  this.entradas=[
+    {titulo:"Pyton cada día mas presente."},
+    {titulo:"Java presente desde hace mas de 20 años."},
+    {titulo:"JavaScript cada vez mas funcional.."},
+    {titulo:"Kotlin potencia para tus apps."},
+    {titulo:"¿Donde quedó pascal?."},
+  ];
+}
+
+
+  registrarUsuario(){
+    this.registro=true;
+    this.mensaje="Usuario registrado con exito";
+  }
 }
